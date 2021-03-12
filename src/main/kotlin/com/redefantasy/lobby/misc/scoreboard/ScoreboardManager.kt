557c52bfc58d6df1d747674f9456f3ad12ec5b2b
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-03-12 14:19:50 -0300

    dev

--- src/main/kotlin/com/redefantasy/lobby/misc/scoreboard/ScoreboardManager.kt
+++ src/main/kotlin/com/redefantasy/lobby/misc/scoreboard/ScoreboardManager.kt
@@ -1,8 +1,13 @@
 package com.redefantasy.lobby.misc.scoreboard
 
 import com.redefantasy.core.shared.CoreProvider
+import com.redefantasy.core.shared.users.data.User
+import com.redefantasy.core.shared.users.storage.table.UsersTable
 import com.redefantasy.core.spigot.misc.scoreboard.bukkit.BaseScoreboard
+import com.redefantasy.core.spigot.misc.scoreboard.bukkit.GroupScoreboard
+import org.apache.commons.lang3.StringUtils
 import org.bukkit.entity.Player
+import org.jetbrains.exposed.dao.id.EntityID
 
 /**
  * @author Gutyerrez
@@ -11,7 +16,7 @@ object ScoreboardManager {
 
     fun construct(player: Player) {
         val user = CoreProvider.Cache.Local.USERS.provide().fetchById(player.uniqueId)
-        val groupPrefix = user?.getHighestGroup()?.getFancyDisplayName() ?: "§7Membro"
+        val fancyGroupName = user?.getHighestGroup()?.getFancyDisplayName() ?: "§7Membro"
 
         val scoreboard = BaseScoreboard()
 
@@ -19,25 +24,49 @@ object ScoreboardManager {
 
         scoreboard.setTitle("§6§lREDE FANTASY")
         scoreboard.set(15, "§0")
-        scoreboard.set(14, "§f Online: §a${onlineUsers.size}")
-        scoreboard.set(13, "§1")
+        scoreboard.set(14, "§f Online: §7${onlineUsers.size}")
+        scoreboard.set(13, "§f Grupo: $fancyGroupName")
+        scoreboard.set(12, "§1")
 
-        var i = 12
+        var i = 11
 
         CoreProvider.Cache.Local.SERVERS.provide().fetchAll().forEach {
             val onlineUsers = CoreProvider.Cache.Redis.USERS_STATUS.provide().fetchUsersByServer(it)
 
-            scoreboard.set(i, "§f ${it.displayName}: §e${onlineUsers.size}")
+            scoreboard.set(
+                i, "§f ${
+                    StringUtils.replaceEach(
+                        it.displayName,
+                        arrayOf(
+                            "Rankup",
+                            "Factions"
+                        ),
+                        arrayOf(
+                            "R.",
+                            "F."
+                        )
+                    )
+                }: §e${onlineUsers.size}"
+            )
 
-            i++
+            i--
         }
 
+        val bukkitApplicationName = CoreProvider.application.displayName.split(" ")[1]
+
         scoreboard.set(3, "§2")
-        scoreboard.set(2, "§f Grupo: $groupPrefix")
+        scoreboard.set(2, "§f Saguão: $bukkitApplicationName")
         scoreboard.set(1, "§3")
-        scoreboard.set(0, "§e  redefantasy.com")
+        scoreboard.set(0, "§e  loja.redefantasy.com")
 
         scoreboard.send(arrayOf(player))
+
+        val groupScoreboard = GroupScoreboard()
+
+        groupScoreboard.registerUser(user ?: User(
+            EntityID(player.uniqueId, UsersTable),
+            player.name
+        ))
     }
 
 }
