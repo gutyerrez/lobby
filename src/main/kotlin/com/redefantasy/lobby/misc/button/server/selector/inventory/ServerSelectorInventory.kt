fc3de8b64756eedbd8e5004c7799c9e948904f6f
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-03-14 07:52:19 -0300

    dev

--- src/main/kotlin/com/redefantasy/lobby/misc/button/server/selector/inventory/ServerSelectorInventory.kt
+++ src/main/kotlin/com/redefantasy/lobby/misc/button/server/selector/inventory/ServerSelectorInventory.kt
@@ -10,6 +10,8 @@ import com.redefantasy.core.spigot.misc.utils.ItemBuilder
 import net.md_5.bungee.api.chat.TextComponent
 import org.bukkit.Material
 import org.bukkit.entity.Player
+import org.bukkit.event.inventory.InventoryClickEvent
+import java.util.function.Consumer
 
 /**
  * @author Gutyerrez
@@ -40,45 +42,46 @@ class ServerSelectorInventory : CustomInventory(
                     )
                 )
                 .build(),
-        ) { it ->
-            if (factionsOmegaBukkitSpawnApplication !== null) {
-                val factionsOmegaBukkitSpawnApplicationStatus =
-                    CoreProvider.Cache.Redis.APPLICATIONS_STATUS.provide().fetchApplicationStatusByApplication(
-                        factionsOmegaBukkitSpawnApplication,
-                        ApplicationStatus::class
+            Consumer<InventoryClickEvent> {
+                if (factionsOmegaBukkitSpawnApplication !== null) {
+                    val factionsOmegaBukkitSpawnApplicationStatus =
+                        CoreProvider.Cache.Redis.APPLICATIONS_STATUS.provide().fetchApplicationStatusByApplication(
+                            factionsOmegaBukkitSpawnApplication,
+                            ApplicationStatus::class
+                        )
+
+                    println(factionsOmegaBukkitSpawnApplication)
+
+                    val player = it.whoClicked as Player
+                    val user = CoreProvider.Cache.Local.USERS.provide().fetchById(player.uniqueId)
+
+                    if (factionsOmegaBukkitSpawnApplicationStatus === null) {
+                        println("nullo")
+
+                        player.sendMessage(TextComponent("§cEste servidor está offline."))
+                        return@Consumer
+                    }
+
+                    val json = CoreConstants.GSON.toJson(factionsOmegaBukkitSpawnApplicationStatus)
+
+                    println(" --> $json")
+
+                    val packet = ConnectUserToApplicationPacket(
+                        user?.id,
+                        factionsOmegaBukkitSpawnApplication
                     )
 
-                println(factionsOmegaBukkitSpawnApplication)
+                    println("Enviar o packet")
 
-                val player = it.whoClicked as Player
-                val user = CoreProvider.Cache.Local.USERS.provide().fetchById(player.uniqueId)
-
-                if (factionsOmegaBukkitSpawnApplicationStatus === null) {
-                    println("nullo")
-
-                    player.sendMessage(TextComponent("§cEste servidor está offline."))
-                    return@setItem
+                    CoreProvider.Databases.Redis.ECHO.provide().publishToApplications(
+                        packet,
+                        CoreProvider.Cache.Local.APPLICATIONS.provide().fetchByApplicationType(
+                            ApplicationType.PROXY
+                        )
+                    )
                 }
-
-                val json = CoreConstants.GSON.toJson(factionsOmegaBukkitSpawnApplicationStatus)
-
-                println(" --> $json")
-
-                val packet = ConnectUserToApplicationPacket(
-                    user?.id,
-                    factionsOmegaBukkitSpawnApplication
-                )
-
-                println("Enviar o packet")
-
-                CoreProvider.Databases.Redis.ECHO.provide().publishToApplications(
-                    packet,
-                    CoreProvider.Cache.Local.APPLICATIONS.provide().fetchByApplicationType(
-                        ApplicationType.PROXY
-                    )
-                )
             }
-        }
+        )
     }
 
 }
