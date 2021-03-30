001278ad0780695de6e84858c87f4faac7f45c51
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-03-30 04:15:57 -0300

    dev

--- src/main/kotlin/com/redefantasy/lobby/LobbyPlugin.kt
+++ src/main/kotlin/com/redefantasy/lobby/LobbyPlugin.kt
@@ -23,6 +23,7 @@ import org.bukkit.Bukkit
 import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.craftbukkit.v1_8_R3.CraftWorld
+import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.Giant
 import org.bukkit.event.entity.CreatureSpawnEvent
 import org.bukkit.potion.PotionEffect
@@ -189,7 +190,7 @@ class LobbyPlugin : CustomPlugin(false) {
                     )
                     npc.removeWhenFarAway = false
                     npc.equipment.itemInHand = ItemBuilder(Material.BLAZE_POWDER)
-                        .glowing(true)
+                        .enchant(Enchantment.DURABILITY)
                         .build()
                     npc.teleport(npcLocation.clone().add(1.9, -8.5, -3.5))
                 }
