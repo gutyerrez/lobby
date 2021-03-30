e346759c129682e58360af9e4b6c4003971b9be6
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-03-30 20:53:32 -0300

    dev

--- src/main/kotlin/com/redefantasy/lobby/LobbyPlugin.kt
+++ src/main/kotlin/com/redefantasy/lobby/LobbyPlugin.kt
@@ -264,7 +264,7 @@ class LobbyPlugin : CustomPlugin(false) {
                 Bukkit.getWorld("world"),
                 -3.5,
                 90.0,
-                -39.0
+                -37.7
             ),
             BlockFace.SOUTH
         )
