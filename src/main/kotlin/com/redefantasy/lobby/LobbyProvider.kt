8d16cc6ff7099b74052c1874978ea05e2a0eb414
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-25 00:04:00 -0300

    dev

--- src/main/kotlin/com/redefantasy/lobby/LobbyProvider.kt
+++ src/main/kotlin/com/redefantasy/lobby/LobbyProvider.kt
@@ -1,9 +1,11 @@
 package com.redefantasy.lobby
 
+import com.redefantasy.core.shared.misc.servers.configuration.cache.local.ServersConfigurationLocalCache
 import com.redefantasy.core.shared.providers.cache.local.LocalCacheProvider
 import com.redefantasy.core.shared.providers.cache.redis.RedisCacheProvider
 import com.redefantasy.lobby.misc.queue.cache.redis.QueueRedisCache
 import com.redefantasy.lobby.user.cache.local.LobbyUserLocalCache
+import org.bukkit.inventory.ItemStack
 
 /**
  * @author Gutyerrez
@@ -12,6 +14,7 @@ object LobbyProvider {
 
     fun prepare() {
         Cache.Local.LOBBY_USERS.prepare()
+        Cache.Local.SERVER_CONFIGURATION.prepare()
 
         Cache.Redis.QUEUE.prepare()
     }
@@ -22,6 +25,10 @@ object LobbyProvider {
 
             val LOBBY_USERS = LocalCacheProvider(
                 LobbyUserLocalCache()
+            )
+
+            val SERVER_CONFIGURATION = LocalCacheProvider(
+                ServersConfigurationLocalCache<ItemStack>()
             )
 
         }
