151448e2a6b4329f9807b7bea9a92ea23cdc0ed8
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-05-10 13:16:58 -0300

    dev

--- src/main/kotlin/net/hyren/lobby/listeners/GenericListener.kt
+++ src/main/kotlin/net/hyren/lobby/listeners/GenericListener.kt
@@ -185,6 +185,8 @@ class GenericListener : Listener {
         val player = event.player
         val item = player.itemInHand
 
+        println("Click: ${event.action}")
+
         val clickedBlock = event.clickedBlock
 
         event.isCancelled = true
