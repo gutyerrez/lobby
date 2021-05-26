e9937a6dd498142da2e0bd13dabf185e898ab2d6
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-05-26 17:52:59 -0300

    dev

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,10 +43,8 @@ dependencies {
     compileOnly("io.github.waterfallmc:waterfall-chat:1.16-R0.5-SNAPSHOT")
 
     // exposed
-    compileOnly("org.jetbrains.exposed:exposed-core:0.29.1")
-    compileOnly("org.jetbrains.exposed:exposed-dao:0.29.1")
-    compileOnly("org.jetbrains.exposed:exposed-jdbc:0.29.1")
-    compileOnly("org.jetbrains.exposed:exposed-jodatime:0.29.1")
+    compileOnly("org.jetbrains.exposed:exposed-dao:0.31.1")
+    compileOnly("org.jetbrains.exposed:exposed-jodatime:0.31.1")
 
     // redis
     compileOnly("redis.clients:jedis:3.3.0")
