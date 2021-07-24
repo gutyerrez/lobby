b9b8bd411fa0f11819e18c666229d40de2404aed
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-07-23 21:48:31 -0300

    dev

--- src/main/kotlin/net/hyren/lobby/listeners/GenericListener.kt
+++ src/main/kotlin/net/hyren/lobby/listeners/GenericListener.kt
@@ -9,6 +9,7 @@ import net.hyren.lobby.LobbyPlugin
 import net.hyren.lobby.LobbyProvider
 import net.hyren.lobby.misc.button.HotBarManager
 import net.hyren.lobby.misc.captcha.inventory.CaptchaInventory
+import net.hyren.lobby.misc.scoreboard.ScoreboardManager
 import net.md_5.bungee.api.chat.ComponentBuilder
 import org.bukkit.Bukkit
 import org.bukkit.Location
